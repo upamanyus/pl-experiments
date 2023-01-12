1d75329a2a46d93acb0012161b9a203a2a86cb65
Upamanyu Sharma <upamanyu@mit.edu>
2023-01-12 16:18:26 -0500

    Prove one case of abs in fundamental property theorem; admitted
    substitution lemma.

--- stlc/src/typesafety.lean
+++ stlc/src/typesafety.lean
@@ -101,16 +101,42 @@ begin
   }
 end
 
+def not_in_env (γ:env) (x:string) : Prop := (∀ v, (x, v) ∉ γ)
+
+lemma env_sub_lam_notin :
+∀ γ x τ e,
+  not_in_env γ x →
+  env_sub γ (lam x τ e) = lam x τ (env_sub γ e) :=
+begin
+sorry
+end
+
+lemma env_sub_lam_in :
+∀ γ x τ e v,
+  ((x, v) ∈ γ) →
+  env_sub γ (lam x τ e) = lam x τ e :=
+begin
+sorry
+end
+
+-- This is the "substitution lemma" on page 13 of notes.
+lemma substitution_lemma :
+  ∀ γ x vx e,
+  not_in_env γ x →
+  env_sub ((x,vx)::γ) e = substitute x vx (env_sub γ e) :=
+begin
+sorry
+end
+
 theorem fundamental_property :
 ∀ Γ e τ,
   (mk_context Γ ⊢ e : τ) →
   (Γ ⊨ e : τ) :=
 begin
   introv Hty,
-  unfold semantic_has_type,
   generalize h : (mk_context Γ) = (ctx),
   rw h at *,
-  induction Hty generalizing Γ; subst h,
+  induction Hty generalizing Γ; subst h; unfold semantic_has_type,
   { -- case: unit
     introv Hγ, rw env_sub_unit,
     unfold interp_exp step_closure, simp *,
@@ -131,7 +157,36 @@ begin
   },
   { -- case: abs; this is where induction will be a bit tricky.
     introv Hγ,
-    sorry,
+    rename [Hty_x → x, Hty_τ1 → τ1, Hty_τ2 → τ2, Hty_e → e],
+    -- FIXME: using this false lemma (could be made true modulo α-substitutions)
+    -- to mimic the argument in the lecture notes for now.
+    by_cases (not_in_env γ x), tactic.swap,
+    { sorry },
+    {
+      rw env_sub_lam_notin, tactic.swap, { tauto },
+      intros e' Hstep,
+      cases Hstep with Hstep Hirred,
+      cases Hstep, tactic.swap, { cases Hstep_Hstep },
+      unfold interp_val,
+      split, tactic.swap,
+      { unfold closed_vals, constructor },
+      constructor,
+      intros v Hv,
+      -- NOTE: step_closure V⟦τ2⟧ is the same as E⟦τ2⟧.
+      rw (_:step_closure V⟦τ2⟧ = E⟦τ2⟧), tactic.swap,
+      { unfold interp_exp },
+
+      -- Apply inductive hypothesis
+      specialize Hty_ih ((x,τ1)::Γ) _,
+      { unfold mk_context },
+      specialize Hty_ih ((x,v)::γ) _,
+      { constructor, { refl },
+        split; assumption,
+      },
+      unfold env_sub at Hty_ih,
+      rw <- substitution_lemma, tactic.swap, assumption,
+      apply Hty_ih,
+    }
   },
   repeat { sorry }
 end
